8c4c100dde4b39a5e010504ad7e72e5f79ff40f5
joyceyskwon <joyceyskwon@gmail.com>
2019-05-22 10:36:46 -0400

    added icon

--- src/components/ChatroomItem.js
+++ src/components/ChatroomItem.js
@@ -4,7 +4,7 @@ import { List } from 'semantic-ui-react'
 const ChatroomItem = ({ chatroom }) => {
   return (
     <List divided relaxed>
-      <List.Icon name='github' size='large' verticalAlign='middle' />
+      <List.Icon name='comments' size='large' verticalAlign='middle' />
       <List.Content>
         <List.Header as='a'>{chatroom.subject}</List.Header>
         <List.Description as='a'>Updated 10 mins ago</List.Description>
